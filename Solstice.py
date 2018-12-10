f09f5e0857864b8bde345e9411d57c97789662f8
rajeevgopalakrishna <rgk@zenviron.io>
2018-12-10 18:26:54 +0800

    Added license notice

--- Solstice.py
+++ Solstice.py
@@ -1,3 +1,20 @@
+#	Copyright (c) 2018 Rajeev Gopalakrishna
+#
+#	This file is part of Solstice.
+#
+#	Solstice is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	Solstice is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with Solstice.  If not, see <http://www.gnu.org/licenses/>.
+
 import subprocess, os, sys, getopt, logging
 from ParseAST.parseAST import ParseAST
 from Analysers.analyseExternalContractInteractions import AnalyseExternalContractInteractions
